fe1572b9cf0a69675dee3bf8b2efaa6d907ce4c2
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-08 17:02:45 +0900

    2문 완

--- src/com/javaex/ex02/Shape.java
+++ src/com/javaex/ex02/Shape.java
@@ -2,6 +2,27 @@ package com.javaex.ex02;
 
 public class Shape {
 	
+	//배열
+	
+	private String fillColor;
+	private String lineColor;
+	
+	//생성자
+	public Shape() {
+		System.out.println("생성자 Shape(0) 실행");
+	}
+	
+	public Shape(String fillColor, String lineColor) {
+		this.fillColor = fillColor;
+		this.lineColor = lineColor;
+		System.out.println("생성자 Shape(2) 실행");
+	}
+	//메서드 gs
+	
+	//메서드 일반
+	
+	
+	
 
 }
 

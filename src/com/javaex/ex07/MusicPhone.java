0b61532fddc1aba569c4ac06a8d96a6cfef4bbcc
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-08 20:15:00 +0900

    7문 완

--- src/com/javaex/ex07/MusicPhone.java
+++ src/com/javaex/ex07/MusicPhone.java
@@ -2,6 +2,13 @@ package com.javaex.ex07;
 
 public class MusicPhone extends Phone{
     
+	//생성자
+	public MusicPhone() {
+		super();
+	}
+	
+	
+	//메서드 일반
     public void execute(String str){
         
         if("음악".equals(str)){
--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -1,17 +1,37 @@
 package com.javaex.ex07;
 
 public class SmartPhone extends MusicPhone {
-    
-    public void execute(String str){
-        
-        //코드작성
-        
-    }
- 
-    //메소드작성
-    
-    //메소드작성
-    
-    
-    
+
+	// 생성자
+	public SmartPhone() {
+		super();
+	}
+
+	public void execute(String str) {
+
+		if (str.equals("앱")) {
+			app();
+		} else if (str.equals("음악")) {
+			playMusic();
+		} else if(str.equals("통화")) {
+			call();
+		}
+
+	}
+
+	public void app() {
+		System.out.println("앱실행");
+
+	}
+
+	public void playMusic() {
+		System.out.println("다운로드해서 음악재생");
+	}
+	public void call() {
+		System.out.println("통화기능 시작");
+	}
+	// 메소드작성
+
+	// 메소드작성
+
 }

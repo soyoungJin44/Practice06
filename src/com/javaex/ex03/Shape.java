ee1e0ee5340ebda1e40412077bc77395bb783186
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-08 17:27:06 +0900

    3문 완

--- src/com/javaex/ex03/Shape.java
+++ src/com/javaex/ex03/Shape.java
@@ -2,6 +2,36 @@ package com.javaex.ex03;
 
 public class Shape {
 	
+	//배열
+	protected String fillColor;
+	protected String lineColor;
+	
+	//생성자
+	public Shape() {
+		super();
+	}
+	
+	public Shape(String fillColor, String lineColor) {
+		this.fillColor = fillColor;
+		this.lineColor = lineColor;
+	}
+	
+	//메서드 gs
+	public void setFillColor(String fillColor) {
+		this.fillColor = fillColor;
+	}
+	public String getFillColor() {
+		return fillColor;
+	}
+	
+	public void setLineColor(String lineColor) {
+		this.lineColor = lineColor;
+	}
+	public String getLineColor() {
+		return lineColor;
+	}
+	//메서드
+	
 	
 	
 }
--- src/com/javaex/ex03/Triangle.java
+++ src/com/javaex/ex03/Triangle.java
@@ -2,14 +2,19 @@ package com.javaex.ex03;
 
 public class Triangle extends Shape{
 	
+	//배열
 	private int width;
 	private int height;
 	
+	//생성자
 	public Triangle(int width, int height) {
 		this.width = width;
 		this.height = height;
 	}
 	
+	//메서드 gs
+	
+	//메서드 일반
 	public void showInfo() {
 		System.out.println("====삼각형====");
 		System.out.println("#면색:"+fillColor);
